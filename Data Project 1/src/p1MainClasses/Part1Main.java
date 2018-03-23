d5b9e4d4b7231a79d484333a9e3aa9f57c2a6f05
Héctor <hector.montes1@upr.edu>
2018-03-23 14:09:38 -0400

    Bug Fixes

--- Data Project 1/src/p1MainClasses/Part1Main.java
+++ Data Project 1/src/p1MainClasses/Part1Main.java
@@ -61,58 +61,16 @@ public class Part1Main {
 	
 	
 	public static void doSolutionP1() {
-		ArrayList<MySet> setList = new ArrayList<MySet>();
-		
-		//Arranges the data to be analyzed by P1AndP2 with Set1
-		for(int i = 0; i < m; i++) {
-			MySet set = new Set1<>();
-			for(int j = 0; j < n; j++) {
-						
-				for(int k = 0; k<allData[j][i].length; k++) {
-					if(!set.contains(allData[j][i][k])) {
-						set.add(allData[j][i][k]);
-							}
-						}
-					}
-				setList.add(set);			
-			}
-				
-			//Para set1 p1
-		P1AndP2 comparator = new P1AndP2("P1");
 
-		MySet[] setsToVerify = new MySet[m];				
-		setList.toArray(setsToVerify);
-		MySet returnSet = comparator.intersectSets(setsToVerify);
-			
-		System.out.println("Final Set By " + comparator.getName() + ": " + returnSet.toString());
-				
+		P1AndP2 comparator = new P1AndP2("P1");
+		System.out.println("Final Set by " + comparator.getName() + ": " +comparator.intersectSets(comparator.generateSets((Integer[][][]) allData)));
 	}
 	
 	
 	public static void doSolutionP2() {
-		ArrayList<MySet> setList = new ArrayList<MySet>();
-		
-		//another set set2
-		for(int i = 0; i < m; i++) {
-			MySet set = new Set2<>();
-			for(int j = 0; j < n; j++) {
-						
-				for(int k = 0; k<allData[j][i].length; k++) {
-					if(!set.contains(allData[j][i][k])) {
-						set.add(allData[j][i][k]);
-							}
-						}
-					}
-				setList.add(set);			
-			}
-		
-		MySet[] returnSet = new MySet[m];
-		setList.toArray(returnSet);
-				
-				
+			
 		P1AndP2 comparator = new P1AndP2("P2");
-		MySet solution = comparator.intersectSets(returnSet);
-		System.out.println("Final Set By " + comparator.getName() + ": " + solution.toString());			
+		System.out.println("Final Set by " + comparator.getName() + ": " + comparator.intersectSets(comparator.generateSets((Integer[][][]) allData)));
 		
 	}
 	
@@ -121,40 +79,20 @@ public class Part1Main {
 	public static void doSolutionP3() {
 	
 		P3 solutionP3 = new P3("P3");
-		MySet solution = solutionP3.intersectSets(arrangeDataP3P4());
-		System.out.println("Final Set By " + solutionP3.getName() + ": " + solution.toString());		
-		
+		System.out.println("Final Set by " + solutionP3.getName() + ": " + solutionP3.intersectSets(solutionP3.arrangeDataP3P4((Integer[][][]) allData)));
+
 		
 		
 	}
 	public static void doSolutionP4() {
 		
 		P4 solutionP4 = new P4<>("P4");
-		MySet solution = solutionP4.intersectSets(arrangeDataP3P4());
-		System.out.println("Final Set By " + solutionP4.getName() + ": " + solution.toString());		
+		System.out.println("Final Set by " + solutionP4.getName() + ": " + solutionP4.intersectSets(solutionP4.arrangeDataP3P4((Integer[][][]) allData)));
 		
 		
 		
 	}
 	
-	public static MySet[] arrangeDataP3P4() {
-		
-		MySet p3p4Sol = new Set1P3<>();
-		
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < n; j++) {
-				
-				for(int k = 0; k<allData[j][i].length; k++) {					
-						p3p4Sol.add(allData[j][i][k]);					
-				}
-			}						
-		}	
-		
-		MySet[] setp3p4 = new MySet[1];
-		setp3p4[0] = p3p4Sol;
-		
-		return setp3p4;
-		}
 	
 	
 	
